e58db612a5b5aee64c72ebbf943bbb4e3947fd65
ankur-kalita <forgivemeankur11@gmail.com>
2025-09-11 02:13:30 +0530

    composite pattern

---
+++ iterator_and_composite_patterns/composite/Menu.java
@@ -0,0 +1,46 @@
+package iterator_and_composite_patterns.composite;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class Menu extends MenuComponent {
+    List<MenuComponent> menuComponents = new ArrayList<MenuComponent>();
+    String name;
+    String description;
+
+    public Menu(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public void add(MenuComponent menuComponent) {
+        menuComponents.add(menuComponent);
+    }
+
+    public void remove(MenuComponent menuComponent) {
+        menuComponents.remove(menuComponent);
+    }
+
+    public MenuComponent getChild(int i) {
+        return menuComponents.get(i);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void print() {
+        System.out.print("\n" + getName());
+        System.out.println(", " + getDescription());
+        System.out.println("---------------------");
+
+        for (MenuComponent menuComponent : menuComponents) {
+            menuComponent.print();
+        }
+    }
+    
+}
---
+++ iterator_and_composite_patterns/composite/Waitress.java
@@ -0,0 +1,14 @@
+package iterator_and_composite_patterns.composite;
+
+// top-level menu component
+public class Waitress {
+    MenuComponent allMenus;
+
+    public Waitress(MenuComponent allMenus) {
+        this.allMenus = allMenus;
+    }
+
+    public void printMenu() {
+        allMenus.print();
+    }
+}
