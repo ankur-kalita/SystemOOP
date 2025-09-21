34b19aa973962079ef093e2f9fb9a4b78a44a7ed
ankur-kalita <forgivemeankur11@gmail.com>
2025-09-21 21:05:10 +0530

    flywieght pattern

--- flyweight_pattern/Client.java
+++ flyweight_pattern/Client.java
@@ -3,7 +3,7 @@ public class Client {
 	public static void main(String[] args) {
 		int[][] deciduousLocations = {{1, 1}, {33, 50}, {100, 90}};
 		int[][] coniferLocations = {{10, 87}, {24, 76}, {2, 64}};
-		TreeFactory treeFactory = new TreeFactory(); // creates the two flyweights
+		TreeFactory treeFactory = new TreeFactory(); // creates two flyweights
 		Tree d, c;
 		try {
 			d = treeFactory.getTree("deciduous");
